e99beb8548b78333ab0ffe54d207877291d9e010
nick0721 <opp01364@gmail.com>
2016-09-08 17:18:33 +0800

    0908-1

---
+++ YUWieBo/Classes/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  YUWieBo
+//
+//  Created by 游宗翰 on 2016/9/8.
+//  Copyright © 2016年 han. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
+

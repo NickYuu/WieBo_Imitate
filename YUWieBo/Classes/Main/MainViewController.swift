fa9ef0a7819e8d209473cf6add4888d257e5a25d
nick0721 <opp01364@gmail.com>
2016-09-08 22:00:54 +0800

    0908 Tools extension

--- YUWieBo/Classes/Main/MainViewController.swift
+++ YUWieBo/Classes/Main/MainViewController.swift
@@ -11,20 +11,13 @@ import UIKit
 class MainViewController: UITabBarController {
     
     // MARK:- 延遲加載屬性
-    lazy var imageName = ["tabbar_home", "tabbar_message_center", "", "tabbar_discover", "tabbar_profile"]
-    lazy var composeButton : UIButton = UIButton()
+    fileprivate lazy var composeButton : UIButton = UIButton(imageName: "tabbar_compose_icon_add", backgroundName: "tabbar_compose_button")
     
-    
+    // MARK:- 系統調用函式
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupComposeButton()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        setupTabBarItems()
     }
 }
 
@@ -32,34 +25,11 @@ class MainViewController: UITabBarController {
 extension MainViewController {
     
     /// 設置發布按鈕
-    func setupComposeButton() {
-        composeButton.setBackgroundImage(UIImage(named: "tabbar_compose_button"), for: .normal)
-        composeButton.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), for: .highlighted)
-        composeButton.setImage(UIImage(named: "tabbar_compose_icon_add"), for: .normal)
-        composeButton.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), for: .highlighted)
-        composeButton.sizeToFit()
+    fileprivate func setupComposeButton() {
+        // 按鈕位置
         composeButton.center = CGPoint(x: tabBar.center.x, y: tabBar.bounds.height/2)
-        
+        // 添加按鈕
         tabBar.addSubview(composeButton)
-    }
-    
-    /// 調整TabBar中的Item
-    func setupTabBarItems() {
-        // MARK:- 增加tabBar選中圖片
-        // 獲取所有Item
-        for i in 0..<tabBar.items!.count{
-            
-            // 獲取Item
-            let item = tabBar.items![i]
-            
-            // 設置中間Item不可使用
-            if i==2 {
-                item.isEnabled = false
-                continue
-            }
-            // 設置其他Item選中的圖片
-            item.selectedImage = UIImage(named: imageName[i] + "_highlighted")
-        }
     }
 }
 
---
+++ YUWieBo/Classes/Tools/extension/UIButton_extension.swift
@@ -0,0 +1,34 @@
+//
+//  UIButton_extension.swift
+//  YUWieBo
+//
+//  Created by 游宗翰 on 2016/9/8.
+//  Copyright © 2016年 han. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    /*
+    class func createButton(imageName:String, backgroundName:String) -> UIButton{
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(named: imageName), for: .normal)
+        button.setBackgroundImage(UIImage(named: imageName + "_highlighted"), for: .highlighted)
+        button.setImage(UIImage(named: backgroundName), for: .normal)
+        button.setImage(UIImage(named: backgroundName + "_highlighted"), for: .highlighted)
+        button.sizeToFit()
+        
+        return button
+    }
+     */
+    convenience init(imageName:String, backgroundName:String) {
+        
+        self.init()
+        
+        self.setImage(UIImage(named: imageName), for: .normal)
+        self.setImage(UIImage(named: imageName + "_highlighted"), for: .highlighted)
+        self.setBackgroundImage(UIImage(named: backgroundName), for: .normal)
+        self.setBackgroundImage(UIImage(named: backgroundName + "_highlighted"), for: .highlighted)
+        self.sizeToFit()
+    }
+}
